9b155271af53d6c924c278a8175cbb689b0e63cb
QuantumPhilosopher <16587451+QuantumPhilosophy@users.noreply.github.com>
2019-04-15 15:07:12 -0700

    change .handlebars to .hbs

--- models/strainReviews.js
+++ models/strainReviews.js
@@ -1,19 +1,19 @@
 'use strict'
 
 module.exports = function (sequelize, DataTypes) {
-    const strain_review = sequelize.define('strain_review', {
-      user_id: {
-        type: DataTypes.INTEGER
-      },
-      strain_id: {
-        type: DataTypes.STRING
-      },
-      strain_review: {
-        type: DataTypes.TEXT
-      },
-      strain_rating: {
-        type: DataTypes.INTEGER
-      }
-    })
-    return strain_review
-  }
+  const strain_review = sequelize.define('strain_review', {
+    user_id: {
+      type: DataTypes.INTEGER
+    },
+    strain_id: {
+      type: DataTypes.STRING
+    },
+    strain_review: {
+      type: DataTypes.TEXT
+    },
+    strain_rating: {
+      type: DataTypes.INTEGER
+    }
+  })
+  return strain_review
+}
