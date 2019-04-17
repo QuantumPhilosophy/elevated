e35e6f005e9bba214039f0281bc96710fddc961c
nkanderson86 <47170221+nkanderson86@users.noreply.github.com>
2019-04-17 14:12:58 -0700

    Na/html routes 2 (#36)
    * adding prelim html routes
    
    * wip
    
    * pushing what I have before diving into modal
    
    * adding new bev.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,34 @@
 'use strict'
 
 const db = require('../models')
+var isAuthenticated = require('../config/middleware/isAuthenticated')
 
 module.exports = function (app) {
+  // Load strains page to view all strains
+  app.get('/strains', isAuthenticated, function (req, res) {
+    db.strain.findAll({}).then(function (strains) {
+      res.render('strains', {
+
+      })
+    })
+  })
+
+  // Load bevs page to view all bevs
+  app.get('/bevs', isAuthenticated, function (req, res) {
+    db.strain.findAll({}).then(function (strains) {
+      res.render('bevs', {
+
+      })
+    })
+  })
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get('/members', isAuthenticated, function (req, res) {
+    res.render('members', {
+
+    })
+  })
+
   // Render 404 page for any unmatched routes
   app.get('*', function (req, res) {
     res.render('404')
