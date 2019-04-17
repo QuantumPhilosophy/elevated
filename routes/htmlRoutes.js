1a2ea1364de155db666b59d07d6e6af8f03beee2
QuantumPhilosopher <16587451+QuantumPhilosophy@users.noreply.github.com>
2019-04-17 13:03:59 -0700

    handlebars reorg

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,29 +3,6 @@
 const db = require('../models')
 
 module.exports = function (app) {
-  // Load index page
-  app.get('/', function (req, res) {
-    db.bev_review.findAll({}).then(function (bevReview) {
-      res.render('index', {
-        msg: 'Welcome to Elevate!',
-        reviews: bevReview
-      })
-    })
-  })
-
-  // Load example page and pass in an example by id
-  app.get('/review/:id', function (req, res) {
-    db.strain_review.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function (strainRev) {
-      res.render('example', {
-        review: strainRev
-      })
-    })
-  })
-
   // Render 404 page for any unmatched routes
   app.get('*', function (req, res) {
     res.render('404')
